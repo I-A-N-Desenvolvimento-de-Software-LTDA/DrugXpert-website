77ad5ce60ca50751578e0ac738d4438a71c13ccf
Israel Antunes Neto <israneto20@gmail.com>
2024-10-01 01:10:32 -0300

    Versão V 0.0.0

---
+++ src/constants/index.jsx
@@ -0,0 +1,178 @@
+import { BotMessageSquare } from "lucide-react";
+import { BatteryCharging } from "lucide-react";
+import { Fingerprint } from "lucide-react";
+import { ShieldHalf } from "lucide-react";
+import { PlugZap } from "lucide-react";
+import { GlobeLock } from "lucide-react";
+
+import user1 from "../assets/profile-pictures/user1.jpg";
+import user2 from "../assets/profile-pictures/user2.jpg";
+import user3 from "../assets/profile-pictures/user3.jpg";
+import user4 from "../assets/profile-pictures/user4.jpg";
+import user5 from "../assets/profile-pictures/user5.jpg";
+import user6 from "../assets/profile-pictures/user6.jpg";
+
+export const navItems = [
+  { label: "Features", href: "#" },
+  { label: "Workflow", href: "#" },
+  { label: "Pricing", href: "#" },
+  { label: "Testimonials", href: "#" },
+];
+
+export const testimonials = [
+  {
+    user: "John Doe",
+    company: "Stellar Solutions",
+    image: user1,
+    text: "I am extremely satisfied with the services provided. The team was responsive, professional, and delivered results beyond my expectations.",
+  },
+  {
+    user: "Jane Smith",
+    company: "Blue Horizon Technologies",
+    image: user2,
+    text: "I couldn't be happier with the outcome of our project. The team's creativity and problem-solving skills were instrumental in bringing our vision to life",
+  },
+  {
+    user: "David Johnson",
+    company: "Quantum Innovations",
+    image: user3,
+    text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.",
+  },
+  {
+    user: "Ronee Brown",
+    company: "Fusion Dynamics",
+    image: user4,
+    text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!",
+  },
+  {
+    user: "Michael Wilson",
+    company: "Visionary Creations",
+    image: user5,
+    text: "I am amazed by the level of professionalism and dedication shown by the team. They were able to exceed our expectations and deliver outstanding results.",
+  },
+  {
+    user: "Emily Davis",
+    company: "Synergy Systems",
+    image: user6,
+    text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.",
+  },
+];
+
+export const features = [
+  {
+    icon: <BotMessageSquare />,
+    text: "Drag-and-Drop Interface",
+    description:
+      "Speed up your research process DrugXpert’s drag-and-drop interface lets you build, analyze, and innovate effortlessly!",
+  },
+  {
+    icon: <Fingerprint />,
+    text: "Multi-Platform Compatibility",
+    description:
+      "Your research doesn’t stop; neither does DrugXpert! Access our tools from any device, anytime, anywhere.",
+  },
+  {
+    icon: <ShieldHalf />,
+    text: "Built-in Templates",
+    description:
+      "From data collection to reporting, DrugXpert’s templates provide the structure you need to stay organized and productive!",
+  },
+  {
+    icon: <BatteryCharging />,
+    text: "Real-Time Preview",
+    description:
+      "Stay ahead of the curve with DrugXpert’s Real-Time Preview—view drug data and research insights as they happen!",
+  },
+  {
+    icon: <PlugZap />,
+    text: "Collaboration Tools",
+    description:
+      "Break down barriers in drug research: DrugXpert's collaboration tools foster teamwork and amplify results!",
+  },
+  {
+    icon: <GlobeLock />,
+    text: "Analytics Dashboard",
+    description:
+      "Elevate your research with real-time data visualization DrugXpert’s Analytics Dashboard gives you the full picture!",
+  },
+];
+
+export const checklistItems = [
+  {
+    title: "Uncover Insights, Elevate Discoveries",
+    description:
+      "Gain a clear vision of your research progress with DrugXpert's intuitive analytics dashboard. Visualize key metrics, track performance, and make data-driven decisions that fuel innovation.",
+  },
+  {
+    title: "Collaborate, Create, Conquer",
+    description:
+      "Bring researchers together across the globe with seamless collaboration tools. Share ideas, insights, and breakthroughs in real-time, driving collective progress in drug discovery.",
+  },
+  {
+    title: "Instant Insights, Immediate Impact",
+    description:
+      "Stay ahead with real-time previews of your research outcomes. Monitor changes and visualize results as they happen, empowering you to act faster and smarter.",
+  },
+  {
+    title: "Simplify the Complex, Accelerate Your Workflow",
+    description:
+      "Experience the ease of use with DrugXpert's drag-and-drop interface. Whether you're analyzing datasets or building reports, streamline your process and focus on what matters most—discovery.",
+  },
+];
+
+export const pricingOptions = [
+  {
+    title: "Free",
+    price: "$0",
+    features: [
+      "Private board sharing",
+      "5 Gb Storage",
+      "Web Analytics",
+      "Private Mode",
+    ],
+  },
+  {
+    title: "Pro",
+    price: "$10",
+    features: [
+      "Private board sharing",
+      "10 Gb Storage",
+      "Web Analytics (Advance)",
+      "Private Mode",
+    ],
+  },
+  {
+    title: "Enterprise",
+    price: "$200",
+    features: [
+      "Private board sharing",
+      "Unlimited Storage",
+      "High Performance Network",
+      "Private Mode",
+    ],
+  },
+];
+
+export const resourcesLinks = [
+  { href: "#", text: "Getting Started" },
+  { href: "#", text: "Documentation" },
+  { href: "#", text: "Tutorials" },
+  { href: "#", text: "API Reference" },
+  { href: "#", text: "Community Forums" },
+];
+
+export const platformLinks = [
+  { href: "#", text: "Features" },
+  { href: "#", text: "Supported Devices" },
+  { href: "#", text: "System Requirements" },
+  { href: "#", text: "Downloads" },
+  { href: "#", text: "Release Notes" },
+];
+
+export const communityLinks = [
+  { href: "#", text: "Events" },
+  { href: "#", text: "Meetups" },
+  { href: "#", text: "Conferences" },
+  { href: "#", text: "Hackathons" },
+  { href: "#", text: "Jobs" },
+];
