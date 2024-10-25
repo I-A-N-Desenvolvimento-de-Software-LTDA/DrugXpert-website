2e0217265a374c9d163e749a6d505c979f7dcc82
Israel Antunes Neto <israneto20@gmail.com>
2024-10-25 18:07:19 -0300

    Google Analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-831CQ2QH9P");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
